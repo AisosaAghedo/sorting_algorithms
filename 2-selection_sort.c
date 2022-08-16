47a31c2b2ff95fdaa383b498390d316035905ab0
Aisosa <allysonaghedo@gmail.com>
2022-08-16 15:33:43 +0100

    selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
- * selection_sort - function that sorts an array of integers in ascending order using
- *the Selection sort algorithm
+ * selection_sort - function that sorts an array of integers in ascending order
+ * using the Selection sort algorithm
  * @array: the array to be sorted
  * @size: size of the array
  */
